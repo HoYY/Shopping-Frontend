32b3284c64b53c0492b22b855fb6e9dafd6161db
HoYY <hoyong8532@naver.com>
2020-04-20 22:44:30 +0900

    spring boot 연동 완료 / todo: jwt, 화원가입, 로그인

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -4,11 +4,26 @@ import '../../styles/utils.scss';
 import mail from './mail.png';
 import lock from './lock.png';
 
-function requestToLogin(idReference, pwReference) {
-    const id = idReference.current;
-    const pw = pwReference.current;
+function handleSubmit(idReference, pwReference, event) {
+    event.preventDefault();
 
-    //Logic
+    const loginUser = {
+        id: idReference.current.value,
+        password: pwReference.current.value
+    }
+
+    fetch('http://127.0.0.1:8080/login', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(loginUser)
+    })
+    .then(response =>
+        response.json().then(json => {
+            console.log(json);
+        })
+    );
 }
 
 function LoginForm() {
@@ -16,11 +31,13 @@ function LoginForm() {
     const pwReference = useRef();
 
     return (
-        <form onSubmit={requestToLogin(idReference, pwReference)}>
-            <LoginLabelInput ref={idReference} placeholder={'아이디'}
-                             type={'text'} img={mail} />
-            <LoginLabelInput ref={pwReference} placeholder={'비밀번호'}
-                             type={'password'} img={lock} />
+        <form onSubmit={(event => handleSubmit(idReference, pwReference, event))}>
+            <LoginLabelInput ref={idReference} name={'id'}
+                             placeholder={'아이디'} type={'text'}
+                             img={mail} />
+            <LoginLabelInput ref={pwReference} name={'password'}
+                             placeholder={'비밀번호'} type={'password'}
+                             img={lock} />
             <button type={'submit'} className={'btn space-top pull-right'}>Sign In</button>
         </form>
     );
